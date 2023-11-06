3a1d18cbe9039bd5c72b52f24d085e2767b73c9c
Milind Jain <108269051+milindjain123@users.noreply.github.com>
2023-11-06 20:58:35 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,34 @@
+const MYCART = document.getElementById("MYCART");
+MYCART.addEventListener("click", () => {
+
+    alert("Your cart is empty");
+});
+
+const item1 = document.getElementById("item1");
+item1.addEventListener("click", () => {
+
+    alert("Item added into cart");
+});
+const item2 = document.getElementById("item2");
+item2.addEventListener("click", () => {
+
+    alert("Item added into cart");
+});
+const item3 = document.getElementById("item3");
+item3.addEventListener("click", () => {
+    alert("Item added into cart");
+});
+const item4 = document.getElementById("item4");
+item4.addEventListener("click", () => {
+
+    alert("Item added into cart");
+});
+const item5 = document.getElementById("item5");
+item5.addEventListener("click", () => {
+
+    alert("Item added into cart");
+});
+const item6 = document.getElementById("item6");
+item6.addEventListener("click", () => {
+    alert("Item added into cart");
+});
